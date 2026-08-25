ec35e34dcf6cda42ee77db77caaaeeaa2b7775da
Eric Mills <ericmills71@gmail.com>
2026-08-25 19:20:46 +0000

    fix: show permissions card only to superadmin

--- lib/auth/roles.ts
+++ lib/auth/roles.ts
@@ -21,7 +21,7 @@ export const ROLE_PERMISSIONS: Record<Role, Permissions> = {
   Admin: {
     view: ["*"],
     edit: ["*"],
-    special: ["builder", "role-preview"]
+    special: []
   },
   Manager: {
     view: ["/events", "/promotional-ideas", "/promo-campaigns", "/feature-shows", "/content-assets", "/influencers", "/attendance", "/analytics", "/torch-radio", "/torchtv", "/staff-notes", "/dj-mc-communications", "/dj-mc-communications/schedules", "/dj-mc-communications/promotional-materials", "/dj-mc-communications/stage-announcement-ideas", "/dj-mc-communications/messaging", "/dj-mc-communications/equipment-reports", "/dj-mc-communications/passwords", "/dj-mc-communications/scheduling"],
